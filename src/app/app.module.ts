f3b2b6392e748a15f0fca5048f73ae590719ba70
amirafauzey <amirafauzey94@gmail.com>
2022-11-03 11:58:47 +0800

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PostJobComponent } from './post-job/post-job.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostJobComponent
   ],
   imports: [
     BrowserModule,
