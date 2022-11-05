064a448c915eab1d9156e2d43bcf584c5c017e9f
amirafauzey <amirafauzey94@gmail.com>
2022-11-05 22:30:21 +0800

    add front end UI

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EmployerDashboardComponent } from './employer-dashboard/employer-dashboard.component';
+import { ErrorPageComponent } from './error-page/error-page.component';
+import { FrontPageComponent } from './front-page/front-page.component';
+import { PostJobComponent } from './post-job/post-job.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: FrontPageComponent },
+  { path: 'job/post', component: PostJobComponent },
+  { path: 'job/employerDashboard', component: EmployerDashboardComponent },
+
+
+  { path:'**', component: ErrorPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FrontPageComponent } from './front-page/front-page.component';
 import { PostJobComponent } from './post-job/post-job.component';
+import { ErrorPageComponent } from './error-page/error-page.component';
+import { EmployerDashboardComponent } from './employer-dashboard/employer-dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostJobComponent
+    PostJobComponent,
+    FrontPageComponent,
+    ErrorPageComponent,
+    EmployerDashboardComponent
   ],
   imports: [
     BrowserModule,
