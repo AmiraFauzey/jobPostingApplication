ec3a1670f57cde5616033eb846d7d919c247a45d
amirafauzey <amirafauzey94@gmail.com>
2022-11-09 16:42:25 +0800

    edit multiple choice button

--- src/app/post-job/post-job.component.ts
+++ src/app/post-job/post-job.component.ts
@@ -14,11 +14,14 @@ import { JobWebServiceService } from '../services/job-web-service.service';
 })
 export class PostJobComponent implements OnInit {
   currentStep = 1;
-  JobBenefit: JobBenefitTypes[] = [];
-  JobSchedule: JobScheduleTypes[] = [];
-  JobType: JobTypes[] = [];
+  jobBenefit: JobBenefitTypes[] = [];
+  jobSchedule: JobScheduleTypes[] = [];
+  jobType: JobTypes[] = [];
+  supplementaryType: SupplementalPayTypes[] = [];
   selectedJobTypes: JobTypes[] = [];
-  SupplementaryType: SupplementalPayTypes[] = [];
+  selectedJobSchedule: JobScheduleTypes[] = [];
+  selectedJobBenefit : JobBenefitTypes[] = [];
+  selectedSupplemetaryType: SupplementalPayTypes[] = [];
 
   constructor(private jobWebServiceService: JobWebServiceService) { }
 
@@ -71,19 +74,19 @@ export class PostJobComponent implements OnInit {
     //   this.categories = res;
     // })
     this.jobWebServiceService.getAllJobBenefits().subscribe((res) => {
-      this.JobBenefit = res;
+      this.jobBenefit = res;
     });
 
     this.jobWebServiceService.getAllJobSchedule().subscribe((res) => {
-      this.JobSchedule = res;
+      this.jobSchedule = res;
     });
 
     this.jobWebServiceService.getAllJobSupType().subscribe((res) => {
-      this.SupplementaryType = res;
+      this.supplementaryType = res;
     });
 
     this.jobWebServiceService.getAllJobType().subscribe((res) => {
-      this.JobType = res;
+      this.jobType = res;
     });
   }
 
@@ -91,23 +94,23 @@ export class PostJobComponent implements OnInit {
     this.currentStep = step;
   }
 
-  BenefitTag(tag: any) {
-    if (this.JobBenefit.indexOf(tag) < 0) {
-      this.JobBenefit.push(tag);
+  benefitTag(tag: any) {
+    if (this.selectedJobBenefit.indexOf(tag) < 0) {
+      this.selectedJobBenefit.push(tag);
     } else {
-      this.JobBenefit = this.JobBenefit.filter((t) => t != tag);
+      this.selectedJobBenefit = this.selectedJobBenefit.filter((t) => t != tag);
     }
   }
 
-  ScheduleTag(tag: any) {
-    if (this.JobSchedule.indexOf(tag) < 0) {
-      this.JobSchedule.push(tag);
+  scheduleTag(tag: any) {
+    if (this.selectedJobSchedule.indexOf(tag) < 0) {
+      this.selectedJobSchedule.push(tag);
     } else {
-      this.JobSchedule = this.JobSchedule.filter((t) => t != tag);
+      this.selectedJobSchedule = this.selectedJobSchedule.filter((t) => t != tag);
     }
   }
 
-  TypeTag(tag: any) {
+  typeTag(tag: any) {
     if (this.selectedJobTypes.indexOf(tag) < 0) {
       this.selectedJobTypes.push(tag);
     } else {
@@ -115,11 +118,11 @@ export class PostJobComponent implements OnInit {
     }
   }
 
-  SupplementaryTag(tag: any) {
-    if (this.SupplementaryType.indexOf(tag) < 0) {
-      this.SupplementaryType.push(tag);
+  supplementaryTag(tag: any) {
+    if (this.selectedSupplemetaryType.indexOf(tag) < 0) {
+      this.selectedSupplemetaryType.push(tag);
     } else {
-      this.SupplementaryType = this.SupplementaryType.filter((t) => t != tag);
+      this.selectedSupplemetaryType = this.selectedSupplemetaryType.filter((t) => t != tag);
     }
   }
 }
